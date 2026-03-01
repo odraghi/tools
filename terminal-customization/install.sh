b2114652773cb23a39cf2c826e8c144541a1f331
odraghi <odraghi@gmail.com>
2026-03-01 23:48:12 +0000

    Fix PS1 counting char of tput code..

--- terminal-customization/install.sh
+++ terminal-customization/install.sh
@@ -23,13 +23,18 @@ fi
 
 cp vimrc "$HOME/.vimrc"
 cp bash_aliases "$HOME/.bash_aliases"
-cp profile_themes "$HOME/.profile_themes"
+cp profile.batcat "$HOME/.profile.batcat"
+cp bash_prompt "$HOME/.bash_prompt"
 
 
-LOAD_PROFILE_THEMES='. "$HOME/.profile_themes"'
-grep -q "^$LOAD_PROFILE_THEMES" ~/.profile || echo "$LOAD_PROFILE_THEMES" >> ~/.profile
+LOAD_PROFILE_BATCAT='. "$HOME/.profile.batcat"'
+grep -q "^$LOAD_PROFILE_BATCAT" ~/.profile || echo "$LOAD_PROFILE_BATCAT" >> ~/.profile
+
+LOAD_BASH_PROMPT='. "$HOME/.bash_prompt"'
+grep -q "^$LOAD_BASH_PROMPT" ~/.profile || echo "$LOAD_BASH_PROMPT" >> ~/.profile
 
 echo "INFO: Updated files"
-ls -ltr $HOME/{.vimrc,.bash_aliases,.profile,.profile_themes}
+ls -ltr $HOME/{.vimrc,.bash_aliases,.profile,.profile.batcat,.bash_prompt}
 
 echo -e "\nNOTE: A Nerdfont must be selected your Terminal app (to render font icons in your desktop)"
+echo -e "\nPlease logoff or run: source ~/.profile"
