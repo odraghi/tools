f17d8c87ac1d956b68c33fef722028c714883045
odraghi <odraghi@gmail.com>
2026-04-10 00:24:38 +0200

    Dockerfile alpine-bench

--- alpine-bench/update_and_push_docker_image.sh
+++ alpine-bench/update_and_push_docker_image.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker build -t alpine-bench:latest .
+docker build -t alpine-bench:3 .
+
+docker image tag alpine-bench:latest odraghi/alpine-bench:3
 docker image tag alpine-bench:latest odraghi/alpine-bench:latest
+
+docker push odraghi/alpine-bench:3
 docker push odraghi/alpine-bench:latest
