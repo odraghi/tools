c4941d20e0af203ed74eb51d8b91b4511e594827
odraghi <odraghi@gmail.com>
2026-04-10 16:37:45 +0200

    Add  alpine-bench User: alpine

--- alpine-bench/update_and_push_docker_image.sh
+++ alpine-bench/update_and_push_docker_image.sh
@@ -2,8 +2,8 @@
 
 docker build -t alpine-bench:3 .
 
-docker image tag alpine-bench:latest odraghi/alpine-bench:3
-docker image tag alpine-bench:latest odraghi/alpine-bench:latest
+docker image tag alpine-bench:3 odraghi/alpine-bench:3
+docker image tag alpine-bench:3 odraghi/alpine-bench:latest
 
 docker push odraghi/alpine-bench:3
 docker push odraghi/alpine-bench:latest
