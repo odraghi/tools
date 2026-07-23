4ae43b5b785739aabab838ff85e7c8d32cbd0322
odraghi <odraghi@gmail.com>
2026-07-23 09:57:03 +0000

    How to push a QCOW2 to a registry

--- container-images/qcow2-to-registry/update_and_push_qcow2_image.sh
+++ container-images/qcow2-to-registry/update_and_push_qcow2_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t ubuntu24:3 .
+docker build -t ubuntu24:3 -f ubuntu24.Containerfile .
 
 docker image tag ubuntu24:3 harbor.odraghi.com/ubuntu24:3
 docker image tag ubuntu24:3 harbor.odraghi.com/ubuntu24:latest
